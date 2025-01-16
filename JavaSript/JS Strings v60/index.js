004f055643203f1f52b83b9d79b363fe7feee9a1
JarvisStark24 <dass96852@gmail.com>
2025-01-16 12:02:31 +0530

    strings js done

---
+++ JavaSript/JS Strings v60/index.js
@@ -0,0 +1,28 @@
+console.log("This is a string practice");
+
+let a = "Jarvis";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length);
+
+name = "Tony Stark";
+let friends = "Rhodey, Pepper, Happy";
+// console.log("His name is " + name + " and his friends are " + friends);
+console.log(`His name is ${name} and his friends are ${friends}`);
+
+let b = "Steevs";
+console.log(b.toUpperCase());
+console.log(b.slice(1, 4));
+console.log(b.replace("ee", "eeeee"));
+console.log(b.concat(" is the Captain America"));
+
+let c = "BANNER";
+console.log(c.toLowerCase());
+console.log(c.slice(1));
+console.log(c.replace("B", "D"));
+console.log(c.concat(" is the Hulk"));
